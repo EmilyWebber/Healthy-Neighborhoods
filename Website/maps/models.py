625a20e7e48f10e2374c04d5f04ccee92b3cdb07
Munger Devin Elizabeth <dmunger@cs.uchicago.edu>
2016-03-03 20:09:51 -0600

    Variable name error fixed

--- Website/maps/models.py
+++ Website/maps/models.py
@@ -2,17 +2,27 @@ from __future__ import unicode_literals
 ##from django.db import models
 from django import forms
 import os
+import csv
 
 def get_variables_choices():
 	base_path = os.path.dirname(__file__)
 	file_path = os.path.abspath(os.path.join(base_path, "..", "..", "Analysis/Data/city_health_stats.csv" ))
 
-	with open(file_path) as f:
-		variables = f.readline().strip().split(",")[2:]
+	
+	with open(file_path, "rU") as f:
+		reader = csv.reader(f)
+		variables = f.next().strip().split(",")[2:]
 
-	print(variables)
+		print(variables)
 	temp = [(x,x) for x in variables]
 	return temp
+	'''
+	with open(file_path, 'rU') as f:
+		fields = csv.reader(f)
+		cols =  next(fields)
+	print(cols)
+	return cols[2:]
+	'''
 
 class Variable(forms.Form):
 	variable_choices = get_variables_choices()
