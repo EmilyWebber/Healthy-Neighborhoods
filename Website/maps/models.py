c7b97ae68a50093bc305021c0a512a4e32708474
Munger Devin Elizabeth <dmunger@cs.uchicago.edu>
2016-03-03 19:34:32 -0600

    Reconciling repository

--- Website/maps/models.py
+++ Website/maps/models.py
@@ -1,17 +1,29 @@
 from __future__ import unicode_literals
-from django.db import models
+##from django.db import models
+from django import forms
 import os
 
-class Variable(models.Model):
+def get_variables_choices():
 	base_path = os.path.dirname(__file__)
 	file_path = os.path.abspath(os.path.join(base_path, "..", "..", "Analysis/Data/city_health_stats.csv" ))
 
 	with open(file_path) as f:
 		variables = f.readline().strip().split(",")[2:]
 
+	print(variables)
+	temp = [(x,x) for x in variables]
+	return temp
 
+class Variable(forms.Form):
+	variable_choices = get_variables_choices()
+	variable_1 = forms.ChoiceField(label = "Variable 1", choices = variable_choices)
+	variable_choices.append(("None", "None"))
+	variable_2 = forms.ChoiceField(label = "Variable 2", choices = variable_choices)
+	##= forms.CharField(max_length = 64)
+
+
+'''
 class Neighborhood(models.Model):
-	name = models.CharField(max_length = 200)
-
-def __str__(self):
-	return name
+	pass
+	##name = models.CharField(max_length = 200)
+'''
