094c76141f01e18e9e5ec77a11f9a48480897093
Munger Devin Elizabeth <dmunger@cs.uchicago.edu>
2016-03-10 14:56:15 -0600

    added buttons and documentation page

--- Website/maps/urls.py
+++ Website/maps/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     #url(r'^hello/$', views.hello),
-    url(r'(?i)healthy.*neighborhood', views.healthy_neighborhoods),
+    url(r'(?i)healthy.*neighborhood', views.healthy_neighborhoods, name="home"),
+    url(r'^documentation', views.documentation, name="documentation"),
     url('', views.blank),
 ]
--- Website/maps/views.py
+++ Website/maps/views.py
@@ -21,37 +21,46 @@ def healthy_neighborhoods(request):
 	form = models.Variable()
 	c = {"form": form, "map": False}
 	if request.method == "POST":
-		form = models.Variable(request.POST)
-		if form.is_valid():
-			v1 = form.cleaned_data["variable_1"]
-			v2 = form.cleaned_data["variable_2"]
+		if "ex1" in request.POST:
+			v1 = "Assault (Homicide)"
+			v2 = "Infant Mortality Rate"
+		elif "ex2" in request.POST:
+			v1= "Birth Rate"
+			v2 = "Dependency"
+		elif "ex3" in request.POST:
+			v1 = "Gonorrhea in Females"
+			v2 = "Gonorrhea in Males"
+		else:
+			form = models.Variable(request.POST)
+			if form.is_valid():
+				v1 = form.cleaned_data["variable_1"]
+				v2 = form.cleaned_data["variable_2"]
+		
+		c["variables"] = [v1.lower(), v2.lower()]
 
-			c["variables"] = [v1.lower(), v2.lower()]
+		print("{} : {}".format(v1, v2))
+		c["map"] = True
+		##print(models.get_result_list(v1, v2))
 
-			print("{} : {}".format(v1, v2))
-			c["map"] = True
-			##print(models.get_result_list(v1, v2))
+		if v1 == v2 or v2 == "None":
+			coef, neighborhood_list = (1, models.get_result_list(v1, v2))
+		else:
+			coef, neighborhood_list = models.get_result_list(v1, v2)
 
-			if v1 == v2 or v2 == "None":
-				coef, neighborhood_list = (1, models.get_result_list(v1, v2))
-			else:
-				coef, neighborhood_list = models.get_result_list(v1, v2)
-
-			c["coef"] = '{0:.3f}'.format(coef)
-			print(c["coef"])
+		c["coef"] = '{0:.3f}'.format(coef)
+		print(c["coef"])
 
 			##neighborhood_coord_dict = models.get_neighborhood_dict()
 
 			##print(neighborhood_coord_dict.keys())
 			##print(len(neighborhood_coord_dict.keys()))
 
-
-			models.get_combo_dict(neighborhood_list, neighborhood_coord_dict)
+		models.get_combo_dict(neighborhood_list, neighborhood_coord_dict)
 
 			##print(neighborhood_list)
 			##print(len(neighborhood_list))
 
-			c["neighborhoods"] = neighborhood_coord_dict
+		c["neighborhoods"] = neighborhood_coord_dict
 			
 
 
@@ -59,6 +68,10 @@ def healthy_neighborhoods(request):
 	return render(request, "maps/healthy_neighborhoods_start.html", c)
 	##return HttpResponse("Healthy Neighborhoods")
 
+
+
+def documentation(request):
+	return render(request, "maps/documentation.html")
 
 
 '''
