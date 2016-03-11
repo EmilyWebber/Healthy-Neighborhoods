b04f48ab02c310e8d347be115172a3498f30de61
Munger Devin Elizabeth <dmunger@cs.uchicago.edu>
2016-03-11 16:48:41 -0600

    Began cleaning

--- Website/maps/views.py
+++ Website/maps/views.py
@@ -1,81 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import datetime ##for testing
 
-from . import models
-
-import json
+from . import forms
 
 
-neighborhood_coord_dict = models.get_neighborhood_dict()
+## Initalize dictionary of neighborhood coordinates
+neighborhood_coord_dict = forms.get_neighborhood_dict()
+c = {"neighborhoods": neighborhood_coord_dict}
 
+
+## Blank/unknown site page
 def blank(request):
 	return render(request, "maps/default.html")
-	##HttpResponse("Nothing here.")
 
+
+## Main site page
 def healthy_neighborhoods(request):
-
-	##c = {"variables": models.Variable.variables,}
-	##c = {"form": form,}
-
-	form = models.Variable()
-	c = {"form": form}
+	c["form"] = forms.Variable()
+	
 	if request.method == "POST":
-		if "ex1" in request.POST:
-			v1 = "Below Poverty Level"
-			v2 = "Childhood Blood Lead Level Screening"
-		elif "ex2" in request.POST:
-			v1= "Assault (Homicide)"
-			v2 = "Per Capita Income"
-		elif "ex3" in request.POST:
-			v1 = "Dependency"
-			v2 = "Firearm-related"
+		if "ex_1" in request.POST:
+			var_1 = "Below Poverty Level"
+			var_2 = "Childhood Blood Lead Level Screening"
+		elif "ex_2" in request.POST:
+			var_1= "Assault (Homicide)"
+			var_2 = "Per Capita Income"
+		elif "ex_3" in request.POST:
+			var_1 = "Dependency"
+			var_2 = "Firearm-related"
 		else:
-			form = models.Variable(request.POST)
+			form = forms.Variable(request.POST)
 			if form.is_valid():
-				v1 = form.cleaned_data["variable_1"]
-				v2 = form.cleaned_data["variable_2"]
+				var_1 = form.cleaned_data["variable_1"]
+				var_2 = form.cleaned_data["variable_2"]
 		
-		c["variables"] = [v1.lower(), v2.lower()]
+		c["variables"] = [var_1.lower(), var_2.lower()]
 
 
-		##print(models.get_result_list(v1, v2))
-
-		if v1 == v2 or v2 == "None":
-			coef, neighborhood_list = (1, models.get_result_list(v1, v2))
+		## Case perfectly correlated variables ("None" or same variable selected)
+		if var_1 == var_2 or var_2 == "None":
+			coef, neighborhood_list = (1, forms.get_result_list(var_1, var_2))
 		else:
-			coef, neighborhood_list = models.get_result_list(v1, v2)
+			coef, neighborhood_list = forms.get_result_list(var_1, var_2)
 
 		c["coef"] = '{0:.3f}'.format(coef)
 
-
-			##neighborhood_coord_dict = models.get_neighborhood_dict()
-
-			##print(neighborhood_coord_dict.keys())
-			##print(len(neighborhood_coord_dict.keys()))
-
-		models.get_combo_dict(neighborhood_list, neighborhood_coord_dict)
-
-			##print(neighborhood_list)
-			##print(len(neighborhood_list))
-
-		c["neighborhoods"] = neighborhood_coord_dict
-			
-
+		forms.get_combo_dict(neighborhood_list, neighborhood_coord_dict)
 
 		return render(request, "maps/healthy_neighborhoods.html", c)
 	return render(request, "maps/healthy_neighborhoods_start.html", c)
-	##return HttpResponse("Healthy Neighborhoods")
 
 
-
+## Site ocumentaion page
 def documentation(request):
 	return render(request, "maps/documentation.html")
-
-
-'''
-class VariableSelectionForm(forms.ModelForm):
-	class Meta:
-		model = models.Variable
-		fields = ["variable"]
-'''
